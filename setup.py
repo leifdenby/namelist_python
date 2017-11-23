fdad5d25d8a90c72c53fce3c94fa2124fe5395f7
Dr Leif Denby <leif@denby.eu>
2017-11-23 12:04:49 +0000

    Switch to pytest for tests

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
+import sys
+
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+class PyTest(TestCommand):
+    def run_tests(self):
+        import shlex
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(['tests/'])
+        sys.exit(errno)
 
 setup(
   name = 'namelist_python',
@@ -10,5 +21,7 @@ setup(
   url = 'https://github.com/leifdenby/namelist_python',
   download_url = 'https://github.com/leifdenby/namelist_python/archive/0.1.tar.gz',
   keywords = ['namelist', 'fortran', 'parsing'],
+  cmdclass=dict(test=PyTest),
+  tests_require=['pytest',],
   classifiers = [],
 )
